f0125d442c601beede1bd248edbec74b4afe43e7
christenx <99223047+christen03@users.noreply.github.com>
2023-11-13 13:35:33 -0800

    mongoDB setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,10 +10,17 @@ export default async function Dashboard(){
     const secretMessage = await api.post.getSecretMessage.query();
     const session = await getServerAuthSession();
 
-
     return (
+        <main>
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
         Now you&apos;re logged in and in the dashboard, you can see the secret message: {secretMessage}
     </h1>
+    <Link
+    href={session ? "/api/auth/signout" : "/api/auth/signin"}
+    className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+  >
+    {session ? "Sign out" : "Sign in"}
+  </Link>
+  </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   if(session){
-    redirect("/dashboard")
+    // redirect("/dashboard")
   }
 
   return (
