eead4a7ba1cd59fec205aafcd7c8f4b9791c61a9
christenx <99223047+christen03@users.noreply.github.com>
2024-01-05 14:11:38 -0800

    dummy push

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -12,7 +12,7 @@ import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
+ * object and keep type safety. s
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
