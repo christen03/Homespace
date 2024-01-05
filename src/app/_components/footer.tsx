ee57c1b4e5226c78b27047f7f2c022058de839d9
Akshat30 <akshat30@gmail.com>
2024-01-05 15:24:59 -0800

    fix mobile

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -16,25 +16,20 @@ function Footer() {
       <div className="">
         <div className="inline-flex w-full items-center justify-center gap-[33px] border-t border-gray-300 bg-white bg-opacity-70 ">
           <div className="flex items-center justify-center">
-            <Link href="/dashboard" className="flex items-center justify-center">
+            <Link href="/" className="flex items-center justify-center">
               <div className="font-regular ml-4 sm:ml-20 flex flex-row text-md leading-normal text-gray-900">
                 <FaCopyright className="mt-1" /> &nbsp; 2024 Homespace &bull;
               </div>
             </Link>
-            <Link href="/dashboard" className="flex items-center justify-center">
+            <Link href="/" className="flex items-center justify-center">
               <div className="font-regular flex flex-row text-md leading-normal text-gray-900">
-                &nbsp; Terms &nbsp;&bull;
-              </div>
-            </Link>
-            <Link href="/dashboard" className="flex items-center justify-center">
-              <div className="font-regular flex flex-row text-md leading-normal text-gray-900">
-                &nbsp; Sitemap
+                &nbsp; Terms
               </div>
             </Link>
           </div>
           <div className="flex shrink grow  basis-0 items-start justify-end gap-6">
             <div className="flex items-center justify-center gap-1 py-3 mr-4 sm:mr-20">
-              <Link href="/dashboard" className="flex items-center justify-center">
+              <Link href="/about" className="flex items-center justify-center">
                 <div className="font-regular flex flex-row text-md leading-normal text-gray-900">
                   About
                 </div>
