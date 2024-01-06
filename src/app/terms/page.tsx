7599b6cb86bbcdb97b12b390ce9ea6a035c234b2
Akshat30 <akshat30@gmail.com>
2024-01-06 14:15:57 -0800

    fixed apostrophe for build

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -10,13 +10,13 @@ export default function Terms() {
             Terms of Service
           </h1>
           <p className="font-source-sans-pro mx-auto mb-8 max-w-screen-lg px-8 text-center text-xl font-medium leading-tight text-white sm:px-0 sm:text-2xl">
-            Don't worry about the terms of service pookie, we are still in
-            development. Here's a photo of one of our engineers Christen taking
-            a break from working so hard on Homespace :{")"}.
+            Don&apos;t worry about the terms of service pookie, we are still in
+            development. Here&apos;s a photo of one of our engineers Christen
+            taking a break from working so hard on Homespace :{")"}.
           </p>
         </div>
       </div>
-      <div className="mx-10 mt-4 pb-10 flex items-center justify-center sm:mx-20">
+      <div className="mx-10 mt-4 flex items-center justify-center pb-10 sm:mx-20">
         <div className="rounded-2xl border-4 border-gray-600 shadow-lg">
           <Image alt="demo" src={christen} />
         </div>
