657cc9b413679013ddbd66678974698740be9c09
Akshat30 <akshat30@gmail.com>
2024-01-06 16:26:49 -0800

    removed gray background

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
   return (
     <main>
       <div className="flex w-full flex-row">
-        <div className="w-1/4 bg-gray-100">
+        <div className="w-1/4 bg-white border-r">
           {/* <EnterAddress setSelected={setSelected} /> */}
           <div className="ml-16 mr-16 mt-12">
             <h3>Type of Room</h3>
@@ -66,10 +66,10 @@ const Dashboard = () => {
             </CustomButton>
           </div>
           <div className="ml-8 mr-8 mt-12 flex grid grid-cols-2 gap-4">
-            <button className="mt-4 flex flex-row items-center justify-center rounded-3xl border-2 border-secondary px-6 py-2 text-center text-sm text-gray-600 ring  ring-gray-200 ring-opacity-50 transition duration-300 hover:scale-[1.02]">
+            <button className="mt-4 flex flex-row items-center justify-center rounded-3xl border-2 border-secondary px-6 py-2 text-center text-sm text-gray-600 transition duration-300 hover:scale-[1.02]">
               Clear Filters
             </button>
-            <button className="mt-4 flex flex-row items-center justify-center rounded-3xl bg-secondary px-6 py-2 text-center text-sm text-white  ring ring-gray-200 ring-opacity-50 transition duration-300 hover:scale-[1.02]">
+            <button className="mt-4 flex flex-row items-center justify-center rounded-3xl bg-secondary px-6 py-2 text-center text-sm text-white transition duration-300 hover:scale-[1.02]">
               Show Results
             </button>
           </div>
